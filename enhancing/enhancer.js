6223807020cb4592ee3a92ae51cc5fb81c558b26
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-08 16:01:12 -0700

    finished mvp for enhancer.js and enhancer.spec.js

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,85 @@ module.exports = {
   get,
 };
 
-function succeed(item) {
-  return { ...item };
-}
 
-function fail(item) {
-  return { ...item };
-}
-
+//durability should be 100
+//accepts item object
+// must return a new item (immutable)
 function repair(item) {
-  return { ...item };
+  const newItem = { ...item };
+  newItem.durability = 100;
+  // console.log(item, '\n', newItem)
+  return newItem;
 }
 
+//item object
+// return new item (immutable)
+// item enhancement succeeds
+function succeed(item) {
+  const newItem = { ...item };
+  if (typeof newItem.enhancement !== "number") {
+    newItem.enhancement = 0;
+    return { ...newItem };
+  }
+  if (newItem.enhancement < 0) {
+    newItem.enhancement = 0;
+    return { ...newItem };
+  } else if (newItem.enhancement > 19) {
+    newItem.enhancement = 20;
+    return { ...newItem };
+  } else {
+    newItem.enhancement = newItem.enhancement + 1;
+    return { ...newItem };
+  }
+  console.log(newItem);
+}
+
+//item object
+// return new item object
+// item enhancement failure
+function fail(item) {
+  const newItem = { ...item };
+  if (typeof newItem.enhancement !== "number") {
+    newItem.enhancement = 0;
+    newItem.durability = 0;
+    return { ...newItem };
+  }
+  if (typeof newItem.durability !== "number") {
+    newItem.enhancement = 0;
+    newItem.durability = 0;
+    return { ...newItem };
+  }
+  if (newItem.enhancement < 0) {
+    newItem.enhancement = 0;
+  }
+  if (newItem.enhancement < 15 && newItem.durability > 0) {
+    if (newItem.durability < 5) {
+      newItem.durability = 0;
+    } else {
+      newItem.durability = newItem.durability - 5;
+    }
+  } else {
+    if (newItem.durability < 10) {
+      newItem.durability = 0;
+    } else {
+      newItem.durability = newItem.durability - 10;
+    }
+  }
+  if (newItem.enhancement > 16) {
+    if (newItem.enhancement > 20) {
+      newItem.enhancement = 20;
+    }
+    newItem.enhancement = newItem.enhancement - 1;
+  }
+
+  return { ...newItem };
+}
+
+
+
+// ---------------------------------------------------------------------------------------
+
+// for stretch
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,50 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe('repair()', () => {
+
+  it('should return item with 100 durability', () => {
+    expect(enhancer.repair({ sword: "excaliber", durability: 80, enhancement: 10 })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 10 })
+    expect(enhancer.repair({ sword: "excaliber", durability: 0, enhancement: 10 })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 10 })
+    expect(enhancer.repair({ sword: "excaliber", durability: -10, enhancement: 10 })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 10 })
+  })
+
+})
+
+describe('succeed()', () => {
+  it('should return item with 100 durability', () => {
+    expect(enhancer.succeed({ sword: "excaliber", durability: 100, enhancement: 10 })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 11 })
+    expect(enhancer.succeed({ sword: "excaliber", durability: 100, enhancement: 0 })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 1 })
+    expect(enhancer.succeed({ sword: "excaliber", durability: 100, enhancement: -10 })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 0 })
+    expect(enhancer.succeed({ sword: "excaliber", durability: 100, enhancement: 20 })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 20 })
+    expect(enhancer.succeed({ sword: "excaliber", durability: 100, enhancement: 45 })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 20 })
+    expect(enhancer.succeed({ sword: "excaliber", durability: 100, enhancement: 'hello' })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 0 })
+    expect(enhancer.succeed({ sword: "excaliber", durability: 100, enhancement: true })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 0 })
+    expect(enhancer.succeed({ sword: "excaliber", durability: 100, enhancement: false })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 0 })
+    expect(enhancer.succeed({ sword: "excaliber", durability: 100, enhancement: '10' })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 0 })
+  })
+})
+
+describe('fail()', () => {
+
+  it('should return item with 100 durability', () => {
+    expect(enhancer.fail({ sword: "excaliber", durability: 100, enhancement: 10 })).toMatchObject({ sword: "excaliber", durability: 95, enhancement: 10 })
+    expect(enhancer.fail({ sword: "excaliber", durability: 100, enhancement: 0 })).toMatchObject({ sword: "excaliber", durability: 95, enhancement: 0 })
+
+    expect(enhancer.fail({ sword: "excaliber", durability: 100, enhancement: 20 })).toMatchObject({ sword: "excaliber", durability: 90, enhancement: 19 })
+    expect(enhancer.fail({ sword: "excaliber", durability: 100, enhancement: 17 })).toMatchObject({ sword: "excaliber", durability: 90, enhancement: 16 })
+
+    expect(enhancer.fail({ sword: "excaliber", durability: 100, enhancement: -20 })).toMatchObject({ sword: "excaliber", durability: 95, enhancement: 0 })
+    expect(enhancer.fail({ sword: "excaliber", durability: 100, enhancement: 25 })).toMatchObject({ sword: "excaliber", durability: 90, enhancement: 19 })
+    expect(enhancer.fail({ sword: "excaliber", durability: 100, enhancement: 'hello' })).toMatchObject({ sword: "excaliber", durability: 0, enhancement: 0 })
+    expect(enhancer.fail({ sword: "excaliber", durability: 100, enhancement: true })).toMatchObject({ sword: "excaliber", durability: 0, enhancement: 0 })
+
+    expect(enhancer.fail({ sword: "excaliber", durability: "hello", enhancement: 0 })).toMatchObject({ sword: "excaliber", durability: 0, enhancement: 0 })
+  })
+
+})
+
+describe('get()', () => {
+  it('should should return a new item with name having "name +<enhancement-level>"', () => {
+    expect(enhancer.get({ sword: "excaliber", durability: 100, enhancement: 1 })).toMatchObject({ sword: "excaliber", durability: 100, enhancement: 1 })
+  })
+})
